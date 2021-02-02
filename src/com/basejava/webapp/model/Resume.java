21584eb7181ddcd050c6ffc8d60f8d46d8066eb5
desconocida11 <khalitovaae@yandex.ru>
2021-02-02 21:26:22 +0300

    Lesson03 equals&hashCode, static

--- src/com/basejava/webapp/model/Resume.java
+++ src/com/basejava/webapp/model/Resume.java
@@ -1,9 +1,24 @@
 package com.basejava.webapp.model;
 
+import java.util.Objects;
+
 /**
  * Initial resume class
  */
 public class Resume {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 
     // Unique identifier
     private String uuid;
