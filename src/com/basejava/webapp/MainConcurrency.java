18708eae7dd8361dfa1a6aa477c53e51269b3afd
desconocida11 <khalitovaae@yandex.ru>
2021-03-19 13:10:08 +0300

    HW11 deadlock

--- src/com/basejava/webapp/MainConcurrency.java
+++ src/com/basejava/webapp/MainConcurrency.java
@@ -41,44 +41,10 @@ public class MainConcurrency {
             }
         });
         System.out.println(counter);
-        getDeadlock(mainConcurrency);
     }
 
     private synchronized void inc() {
         counter++;
     }
 
-    private static void getDeadlock(MainConcurrency mainConcurrency) {
-        System.out.println("Deadlock demonstration");
-        final Object lock1 = new Object();
-        final Object lock2 = new Object();
-        Thread thread1 = new Thread(() -> {
-            try {
-                mainConcurrency.getResource(lock1, lock2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        Thread thread2 = new Thread(() -> {
-            try {
-                mainConcurrency.getResource(lock2, lock1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        thread1.start();
-        thread2.start();
-    }
-
-    private void getResource(Object o1, Object o2) throws InterruptedException {
-        synchronized (o1) {
-            System.out.println("o1");
-            System.out.println(Thread.currentThread().getName());
-            Thread.sleep(100);
-            synchronized (o2) {
-                System.out.println(Thread.currentThread().getName());
-                System.out.println("o2");
-            }
-        }
-    }
 }
